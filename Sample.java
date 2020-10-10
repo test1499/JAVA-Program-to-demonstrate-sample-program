4c1258cf6f61f10fcb4816d9c339b1ae9a0dd639
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-10 20:43:35 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,3 +1,13 @@
+/*
+***********************************************************
+
+Topic   : Program to demonstrate a sample program in java.
+Author  : Chpaone09®
+Date    : Oct 10, 2020
+
+************************************************************
+*/
+
 class Sample{
     public static void main(String[] args){
         System.out.println("\n\n\t Hello World");
